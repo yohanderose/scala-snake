b763652351a05c943a4e283ef474a321244bb89f
Yohan de Rose <yohan.derose@agoda.com>
2023-09-19 19:14:22 +0700

    feat: Adding in enum Direction types (UP,DOWN,LEFT,RIGHT) linked to methods that modify the snakes head position. This way we can keep state as well as apply the operation in the same object

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -9,24 +9,36 @@ import scalafx.scene.shape.Rectangle
 import scalafx.scene.layout.GridPane
 import scalafx.scene.text.Text
 import scalafx.animation.AnimationTimer
+import scalafx.scene.input._ // KeyEvent, KeyCode
 
 object SnakeFx extends JFXApp3 {
+
   val (w, h, cellWidth) = (600.0, 600.0, 25.0)
+  object SnakeDirection extends Enumeration {
+    val Up = (x: Double, y: Double) => (x, y - cellWidth)
+    val Down = (x: Double, y: Double) => (x, y + cellWidth)
+    val Left = (x: Double, y: Double) => (x - cellWidth, y)
+    val Right = (x: Double, y: Double) => (x + cellWidth, y)
+  }
+
   var snake: Array[Rectangle] = {
     Array(0, 1, 2).map(i => {
       val bodyPart =
         Rectangle(w / 2 + i * cellWidth, h / 2, cellWidth, cellWidth)
       bodyPart.fill = SeaGreen
       bodyPart
-
     })
   }
 
-  def updateSnake() = {
-    println("tick")
-    for (i <- 0 until snake.length) {
-      snake(i).layoutX = snake(i).layoutX.value - cellWidth
+  def updateSnake(dir: (Double, Double) => (Double, Double)): Unit = {
+    // println("tick")
+    for (i <- (1 until snake.length).reverse) {
+      snake(i).x = snake(i - 1).x()
+      snake(i).y = snake(i - 1).y()
     }
+    val (newX, newY) = dir(snake(0).x(), snake(0).y())
+    snake(0).x = newX
+    snake(0).y = newY
   }
 
   val board = {
@@ -44,18 +56,39 @@ object SnakeFx extends JFXApp3 {
       scene = new Scene {
         fill = AntiqueWhite
         content = snake
+
+        var currDirection = SnakeDirection.Left
+        onKeyPressed = (e: KeyEvent) => {
+          if (e.code == KeyCode.Up)
+            currDirection =
+              if (currDirection != SnakeDirection.Down) SnakeDirection.Up
+              else currDirection
+          else if (e.code == KeyCode.Down)
+            currDirection =
+              if (currDirection != SnakeDirection.Up) SnakeDirection.Down
+              else currDirection
+          else if (e.code == KeyCode.Left)
+            currDirection =
+              if (currDirection != SnakeDirection.Right) SnakeDirection.Left
+              else currDirection
+          else if (e.code == KeyCode.Right)
+            currDirection =
+              if (currDirection != SnakeDirection.Left) SnakeDirection.Right
+              else currDirection
+        }
+
+        var lastTimestamp = System.nanoTime()
+        val frameNumber: Int = 0
+        AnimationTimer(t => {
+          val dt = (t - lastTimestamp) / 1e8 // change in time in seconds
+          if (dt >= 1) {
+            updateSnake(currDirection)
+            frameNumber + 1
+            lastTimestamp = t
+          }
+        }).start
       }
     }
 
-    var lastTimestamp = System.nanoTime()
-    val frameNumber: Int = 0
-    AnimationTimer(t => {
-      val dt = (t - lastTimestamp) / 1e9 // change in time in seconds
-      if (dt >= 1) {
-        updateSnake()
-        frameNumber + 1
-        lastTimestamp = t
-      }
-    }).start
   }
 }
