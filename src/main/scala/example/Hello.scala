b40fd7d32929500e639326e13c0c4d4118da5daa
Yohan de Rose <yohan.derose@agoda.com>
2023-09-20 13:00:33 +0700

    feat: Adding variables and modals to manage score and gamestate

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -10,6 +10,9 @@ import scalafx.scene.layout.GridPane
 import scalafx.scene.text.Text
 import scalafx.animation.AnimationTimer
 import scalafx.scene.input._ // KeyEvent, KeyCode
+import scalafx.scene.text.Font
+import scalafx.scene.text.FontWeight
+import scalafx.scene.text.FontPosture
 
 object SnakeFx extends JFXApp3 {
 
@@ -67,7 +70,29 @@ object SnakeFx extends JFXApp3 {
     snake :+= newBodyPart
   }
 
-  def checkCollisions(dir: (Double, Double) => (Double, Double)): Unit = {
+  def checkCollisions(
+      dir: (Double, Double) => (Double, Double)
+  ): Int = {
+
+    if (
+      snake(0).x() < 0 || snake(0).x() >= w || snake(0).y() < 0 || snake(0)
+        .y() >= h
+    ) {
+      println("wall collision")
+      return -1
+    }
+
+    if (
+      snake
+        .slice(1, snake.length)
+        .exists(bodyPart =>
+          bodyPart.x() == snake(0).x() && bodyPart.y() == snake(0).y()
+        )
+    ) {
+      println("self collision")
+      return -1
+    }
+
     if (
       snake(0).x() == fruit(0).x() &&
       snake(0).y() == fruit(0).y()
@@ -78,7 +103,10 @@ object SnakeFx extends JFXApp3 {
       val (x, y) = randomCoordsInScene()
       fruit(0).x = x
       fruit(0).y = y
+
+      return 1
     }
+    return 0
   }
 
   override def start(): Unit = {
@@ -88,7 +116,26 @@ object SnakeFx extends JFXApp3 {
       height = h
       scene = new Scene {
         fill = AntiqueWhite
-        content = snake ++ fruit
+        var gameOver = false
+        var score = 0
+        val gameOverModal = new Text {
+          font = Font("Arial", FontWeight.ExtraBold, FontPosture.Regular, 48)
+          text = "Game Over"
+          fill = DimGray
+          x = 70
+          y = 70
+          visible = false
+        }
+        val scoreModal = new Text {
+          font = Font("Arial", FontWeight.ExtraBold, FontPosture.Regular, 20)
+          text = "Score: " + score
+          fill = SteelBlue
+          x = 70
+          y = h - 70
+          visible = true
+        }
+
+        content = snake ++ fruit ++ Array(gameOverModal) ++ Array(scoreModal)
 
         var currDirection = SnakeDirection.Left
         onKeyPressed = (e: KeyEvent) => {
@@ -112,15 +159,24 @@ object SnakeFx extends JFXApp3 {
 
         var lastTimestamp = System.nanoTime()
         val frameNumber: Int = 0
-        AnimationTimer(t => {
-          val dt = (t - lastTimestamp) / 1e8 // change in time in seconds
-          if (dt >= 1) {
-            updateSnake(currDirection)
-            checkCollisions(currDirection)
-            content = snake ++ fruit
-            frameNumber + 1
-            lastTimestamp = t
+        val animator = AnimationTimer(t => {
+
+          if (gameOver) {
+            gameOverModal.visible = true
+          } else {
+            val dt = (t - lastTimestamp) / 1e8 // change in time in seconds
+            if (dt >= 1) {
+              updateSnake(currDirection)
+              val result = checkCollisions(currDirection)
+              if (result < 0) gameOver = true
+              score += result
+              frameNumber + 1
+              lastTimestamp = t
+            }
           }
+
+          scoreModal.text = "Score: " + score
+          content = snake ++ fruit ++ Array(gameOverModal) ++ Array(scoreModal)
         }).start
       }
     }
